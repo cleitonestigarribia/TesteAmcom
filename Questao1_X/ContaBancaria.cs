ca74a6e7c4525537adadd713857808be45e8cf1c
Cleiton Estigarribia <cleiton@sysmiddle.com.br>
2023-02-01 15:40:56 -0300

    Add project files.

---
+++ Questao1_X/ContaBancaria.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace Questao1
+{
+    public class ContaBancaria
+    {
+
+        public int numero { get; set; }
+        public string titular { get; set; }
+        public double depositoInicial { get; set; }
+
+        public ContaBancaria(int _numero, string _titular, double _depositoInicial = 0)
+        {
+            this.numero = _numero;
+            this.titular = _titular;
+            this.depositoInicial = _depositoInicial;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Movimentacoes(quantia, "C");
+        }
+
+        public void Saque(double quantia)
+        {
+            Movimentacoes(quantia, "D");
+            Movimentacoes(Convert.ToDouble("3,50"), "D");
+        }
+
+        public void Movimentacoes(double quantia, string tipo)
+        {
+            string path = string.Format("{0}\\extrato.txt", Environment.CurrentDirectory);
+
+            StreamWriter texto = new StreamWriter(path, true, Encoding.ASCII);
+            texto.WriteLine("{0}|{1}|{2}|{3}", numero, quantia, (tipo == "C" ? "C" : "D"), DateTime.Now);
+            texto.Close();
+
+        }
+
+        public double Saldo()
+        {
+            double saldo = 0;
+
+            string path = string.Format("{0}\\extrato.txt", Environment.CurrentDirectory);
+
+            if (File.Exists(path))
+            {
+                string[] linhas = File.ReadAllLines(path);
+
+                foreach (string linha in linhas)
+                {
+                    if (Convert.ToInt32(linha.Split('|')[0]) == numero)
+                    {
+                        if (linha.Split('|')[2].ToString() == "C")
+                        {
+                            saldo += saldo = Convert.ToDouble(linha.Split('|')[1]);
+                        }
+                        else if (linha.Split('|')[2].ToString() == "D")
+                        {
+                            saldo -= saldo = Convert.ToDouble(linha.Split('|')[1]);
+                        }
+                    }
+
+                }
+            }
+         
+            return saldo;
+
+        }
+
+        public void CriarConta(ContaBancaria dados)
+        {
+            try
+            {
+                StreamWriter texto = new StreamWriter(string.Format("{0}\\contas.txt", Environment.CurrentDirectory), true, Encoding.ASCII);
+
+                texto.Write(String.Format("Conta:{0}\n", dados.numero));
+                texto.Write(String.Format("Titular:{0}\n", dados.titular));
+                texto.Close();
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        public bool ValidarConta(ContaBancaria dados)
+        {
+            string path = string.Format("{0}\\contas.txt", Environment.CurrentDirectory);
+
+            if (!File.Exists(path))
+            {
+                StreamWriter texto = new StreamWriter(string.Format("{0}\\contas.txt", Environment.CurrentDirectory), true, Encoding.ASCII);
+                texto.Close();
+            }
+
+            string[] linhas = File.ReadAllLines(path);
+            bool validado = false;
+
+            foreach (string linha in linhas)
+            {
+                if (linha.Split(':')[0].ToString() == "Conta")
+                {
+                    if (Convert.ToInt32(linha.Split(':')[1]) == dados.numero)
+                    {
+                        validado = true;
+                    }
+                }
+                if (validado && linha.Split(':')[0].ToString() == "Titular")
+                {
+                    if (linha.Split(':')[1].ToString() == dados.titular)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Questao2_X/Program.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Questao2;
+using RestSharp;
+
+public class Program
+{
+    public static void Main()
+    {
+        string teamName = "Paris Saint-Germain";
+        int year = 2013;
+        int totalGoals = getTotalScoredGoals(teamName, year);
+
+        Console.WriteLine("Team " + teamName + " scored " + totalGoals.ToString() + " goals in " + year);
+
+        teamName = "Chelsea";
+        year = 2014;
+        totalGoals = getTotalScoredGoals(teamName, year);
+
+        Console.WriteLine("Team " + teamName + " scored " + totalGoals.ToString() + " goals in " + year);
+
+        // Output expected:
+        // Team Paris Saint - Germain scored 109 goals in 2013
+        // Team Chelsea scored 92 goals in 2014
+    }
+
+    public static int getTotalScoredGoals(string team, int year)
+    {
+        var data = getDataTeams(team, year, 0);
+        int total = 0;
+
+        for (int i = 1; i <= data.total_pages; i++)
+        {
+            foreach (var item in getDataTeams(team, year, i).data)
+            {
+                total += total = Convert.ToInt32(item.team1goals);
+            }
+        }
+        return total;
+    }
+
+    public static BaseTimes getDataTeams(string team, int year, int page)
+    {
+        try
+        {
+            var client = new RestClient(string.Format("https://jsonmock.hackerrank.com/api/football_matches?year={0}&team1={1}{2}", year, team, (page > 0 ? String.Concat("&page=", page) : "")));
+            var request = new RestRequest();
+            request.Method = Method.Get;
+            request.AddHeader("Content-Type", "application/json");
+
+            var Data = new JsonResult(client.Execute(request));
+            JsonResult dataSerial = new JsonResult(((RestSharp.RestResponseBase)Data.Value).Content);
+
+            BaseTimes teams = JsonConvert.DeserializeObject<BaseTimes>(dataSerial.Value.ToString());
+
+            return teams;
+
+        }
+        catch (Exception ex)
+        {
+
+            throw ex;
+        }
+
+        return null;
+    }
+}
